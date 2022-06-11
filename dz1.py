e73974916bd09803d0951a4b4712563d124a6397
vovkodav21 <vishtalyurii@gmail.com>
2022-06-11 19:43:06 +0300

    2тв part

---
+++ dz1.py
@@ -0,0 +1,18 @@
+# 1. Встановити Python 3.x (або ігнорувати якщо вже встановлено) - done
+# 2. Встановити PyCharm Community - done
+# 3. Ввести в консолі
+#
+#    a. import this - Zen of Python
+#
+#    b. import antigravity - пасхалка, комикс
+#
+#    c. import __hello__ - импорт тестового модуля
+#
+#    d. from __future__ import braces - импорт из модуля будущего
+
+print("*        *     *          *      ********       **     ")
+print("  *    *       *          *      *       *     *  *    ")
+print("    *  *       *          *      *      *       **     ")
+print("     *         *          *      *  *  *        **     ")
+print("     *          *        *       *    *         **     ")
+print("     *            ** ****          *      *     **     ")
